354dac5560d39c98a1bc10e4a2432fbf1e8f3049
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-09-02 16:05:28 +0530

    React.createElement() method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,20 @@ function Greeting() {
   return <h1>My First Component</h1>;
 }
 
+// function Greeting() {
+//   return React.createElement("h2", {}, "hello world");
+// }
+
+// function Greeting() {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", null, "hello world"),
+//     React.createElement("p", {}, "About the world"),
+//     React.createElement("p", {}, "Hi there")
+//   );
+// }
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<Greeting />);
