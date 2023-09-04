2a931f36bf504d32ab10d5c0aa805c34f903ee4d
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-09-04 12:38:30 +0530

    We can create object of css properties and pass that object to style attribute to apply the style provided in object

--- src/index.js
+++ src/index.js
@@ -79,10 +79,17 @@ const Title = () => {
 };
 
 const Author = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem,",
+  };
   return (
-    <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
-      Alexandre Dumas
-    </h4>
+    // <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}>
+    //   Alexandre Dumas
+    // </h4>
+    //? Alternative Approach
+    <h4 style={inlineHeadingStyles}>Alexander Dumas</h4>
   );
 };
 
